9c811407144fbdd0b7d935016d073bfbce3844cc
paarnio <p.aarnio@gmail.com>
2017-04-06 13:01:54 +0300

    WPC: First parsing results

---
+++ TRAXExamples/src/siima/trax/AML_html2owl.java
@@ -0,0 +1,31 @@
+package siima.trax;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class AML_html2owl {
+
+	public static void main(String[] args) throws TransformerException {
+		 TransformerFactory factory = TransformerFactory.newInstance();
+	        
+	        Source xsl = new StreamSource("data/parse_aml_html/html_parser.xsl"); 
+	        Transformer transformer = factory.newTransformer(xsl);
+	        //Source xml 
+	        Source xml = new StreamSource("data/parse_aml_html/aml_html_classes_p1.xml");
+	        
+	        Result result = new StreamResult("data/parse_aml_html/parse_results.xml");
+	        
+	        transformer.transform(xml, result);
+	        System.out.println("HTML PARSING: ---- Transformation done: See parse_aml_html-folder. ----");
+
+	}
+
+}
