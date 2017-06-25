15648da059b1e39971b7be53fc452e7bb130a949
paarnio <p.aarnio@gmail.com>
2017-06-25 14:52:28 +0300

    HP: New XML - JAXB to RDF transformer added (WeatherJavaObjectsToRdf from book code)

---
+++ TransformToRDF/readmePA_SemWebProgramming.txt
@@ -0,0 +1,7 @@
+Hebeler: Sem Web programming Ch9 352-358
+Book code loaded to HP: C:\Users\Pekka Aarnio\Documents\Tutkimus\Oma-tutkimus\SemWeb\book_code_semwebprogramming\allChpLibs\SemWebProgramming\Chapter9\WeatherToRdfWithJaxb
+
+
+
+code loaded from:
+http://eu.wiley.com/WileyCDA/WileyTitle/productCd-047041801X.html
---
+++ TransformToRDF/src/net/semwebprogramming/chapter9/WeatherJavaObjectsToRdf.java
@@ -0,0 +1,95 @@
+package net.semwebprogramming.chapter9;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import net.semwebprogramming.chapter9.weather.CurrentObservation;
+
+public class WeatherJavaObjectsToRdf
+{
+   /**
+    * Program that converts weather XML feed to RDF using
+    * a combination of JAXB and the Java object serializer
+    * @param args
+    *    input xml - The URL of the input Weather XML file
+    *       (http://www.weather.gov/xml/current_obs/KBWI.xml)
+    *    output file - the file name of the output RDF file
+    *       (ouputs in TURTLE format)
+    */
+   public static void main(String[] args)
+   {
+	   /* orig. 
+      if(args.length != 2) 
+      {   
+         System.err.println("Usage: java WeatherJavaObjectsToRdf "
+            + "[input xml] [output file]");
+         return;
+      }
+
+      String xmlFile = args[0];
+      String outputFile = args[1];
+      */
+	   
+	   String xmlFile = "data/weather/KBWI.xml";
+	   String outputFile = "data/weather/KBWIoutput.rdf";
+      try
+      {
+         //get the xml file input
+    	 File myFile=new File(xmlFile);
+    	 
+         //URL xmlFileUrl = new URL(xmlFile);
+    	 URL xmlFileUrl = myFile.toURI().toURL();
+         InputStream xmlFileInputStream = xmlFileUrl.openStream();
+         
+         //create the output file
+         FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+         //unmarshal the information from xml
+         JAXBContext jaxbContext = 
+            JAXBContext.newInstance("net.semwebprogramming.chapter9.weather");
+         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+         CurrentObservation currentObservation = 
+            (CurrentObservation)unmarshaller.unmarshal(xmlFileInputStream);
+
+         JavaObjectRdfSerializer serializer = new JavaObjectRdfSerializer(
+               "http://www.weather.gov/weather#", 
+               "http://www.weather.gov/ontology#", 
+               "TURTLE", 
+               "net.semwebprogramming");
+         
+         Collection<Object> objects = new LinkedList<Object>();
+         objects.add(currentObservation);
+         serializer.serialize(objects, outputStream);
+         outputStream.close();
+         
+         System.out.println("Success.");
+      }
+      catch(MalformedURLException e)
+      {
+         e.printStackTrace();
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+      catch (JAXBException e)
+      {
+         e.printStackTrace();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
+
