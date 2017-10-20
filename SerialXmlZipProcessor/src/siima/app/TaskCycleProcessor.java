67190877fc0c6cc7d36e1acc4f9dd03f5c38091e
paarnio <p.aarnio@gmail.com>
2017-10-20 13:12:00 +0300

    HP: TaskCycleProcessor editing.

--- SerialXmlZipProcessor/src/siima/app/TaskCycleProcessor.java
+++ SerialXmlZipProcessor/src/siima/app/TaskCycleProcessor.java
@@ -2,6 +2,7 @@ package siima.app;
 
 import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,13 +18,110 @@ public class TaskCycleProcessor {
 	private TestCaseContainer test_cc = new TestCaseContainer();
 	private TransformController trans_ctrl = new TransformController();
 	
+	/* Directory and File references 
+	String rdir;
+	String rfile1;
+	String rfile2;
+	String sdir;
+	String sfile1;
+	String sfile2;
+	*/
+	
+	private List<String> dirList = new ArrayList<String>();
+	private List<String> fileList = new ArrayList<String>();
+	private Map<String,Integer> dirKeyIndexMap = new HashMap<String,Integer>();
+	private Map<String,Integer> fileKeyIndexMap = new HashMap<String,Integer>();
+	
+	/* Channels 
+	String studentChannel001 = "";
+	String studentChannel002 = "";
+	String referenceChannel001 = "";
+	String referenceChannel002 = "";
+	String mergeChannel001 = "";
+	String mergeChannel002 = "";
+	*/
+	
+	private List<String> channelList = new ArrayList<String>();
+	private Map<String,Integer> channelKeyIndexMap = new HashMap<String,Integer>();
+	
+	/* Constructor */
 	public TaskCycleProcessor(String studentsExcel){
 		excel_mng = new ExcelMng("data/excel/students.xlsx");
+		
+		dirKeyIndexMap.put("stuDir", 0); // temp 
+		dirKeyIndexMap.put("stuDir2", 1);
+		dirKeyIndexMap.put("refDir", 2); // temp 
+		dirKeyIndexMap.put("refDir2", 3);
+		
+		fileKeyIndexMap.put("stuFile1", 0);
+		fileKeyIndexMap.put("stuFile2", 1);
+		fileKeyIndexMap.put("refFile1", 2);
+		fileKeyIndexMap.put("refFile2", 3);
+		
+		channelKeyIndexMap.put("stuC001", 0); //studentChannel
+		channelKeyIndexMap.put("stuC002", 1);
+		channelKeyIndexMap.put("refC001", 2); //referenceChannel
+		channelKeyIndexMap.put("refC002", 3);
+		channelKeyIndexMap.put("merC001", 4); //mergeChannel
+		channelKeyIndexMap.put("merC002", 5);
+		
+		 channelList.add("");
+		 channelList.add("");
+		 channelList.add("");
+		 channelList.add("");
+		 channelList.add("");
+		 channelList.add("");
 	}
+	
+	public String getChannelStringValue(String channelKey){
+		/* 
+		 **/
+		String value = null;
+		Integer channelIdx = channelKeyIndexMap.get(channelKey);
+		value = channelList.get(channelIdx.intValue());
+		return value;	
+	}
+	
+	public void setChannelStringValue(String channelKey, String value){
+		/* 
+		 **/
+		Integer channelIdx = channelKeyIndexMap.get(channelKey);
+		channelList.set(channelIdx.intValue(), value);
+			
+	}
+	
+	public String operationParameterValue(String parameter){
+		/* TODO: Getting Operation parameter's target value
+		 * now only filepath build
+		 **/
+		String value = null;
+							
+		String[] ppath = parameter.split("/");
+		if(ppath.length>1){ // filepath?
+			String dirKey = ppath[0];
+			String fileKey = ppath[1];
+			System.out.println("          Operation parameter 1 keys: " + dirKey + "/" + fileKey);					
+			Integer dirIdx = dirKeyIndexMap.get(dirKey);
+			Integer fileIdx = fileKeyIndexMap.get(fileKey);
+			System.out.println("          Operation parameter 1 Indexes: " + dirIdx + "/" + fileIdx);	
+			
+			if(dirList.get(dirIdx.intValue()).endsWith("/")){
+				 value = dirList.get(dirIdx.intValue()) + fileList.get(fileIdx.intValue());
+				} else {
+					 value = dirList.get(dirIdx.intValue()) + "/" + fileList.get(fileIdx.intValue());					
+				}
+			
+		} else {// Channel?		
+		}
+		
+		
+		return value;
+	}
+	
 	
 	public void runTaskCycles() {
 		/*
-		 *  
+		 *  TODO: read reference files from a reference ZIP
 		 */
 		String resultFileDir = "data/zips";
 		
@@ -43,21 +141,36 @@ public class TaskCycleProcessor {
 		/* --- TestCase Loop --- */
 		for (TestCaseType tcase : testcases) {
 			System.out.println("--+ TestCase Loop --- ");
-			String rdir= tcase.getRefDir();
-			String rfile1= tcase.getRefFile1();
-			String rfile2= tcase.getRefFile2();
+			
 			String sdir= tcase.getStuDir();
 			String sfile1= tcase.getStuFile1();
 			String sfile2= tcase.getStuFile2();
 			
-			//Channels
-			String studentChannel001 = null;
-			String studentChannel002 = null;
-			String referenceChannel001 = null;
-			String referenceChannel002 = null;
-			String mergeChannel001 = null;
-			String mergeChannel002 = null;
+			String rdir= tcase.getRefDir();
+			String rfile1= tcase.getRefFile1();
+			String rfile2= tcase.getRefFile2();
+			 
 			
+			 dirList.add(sdir);
+			 dirList.add(""); //for stuDir2
+			 dirList.add(rdir);
+			 dirList.add(""); //for refDir2
+			 
+			 fileList.add(sfile1);
+			 fileList.add(sfile2);
+			 fileList.add(rfile1);
+			 fileList.add(rfile2);
+			 
+			/*Channels
+			 studentChannel001 = "";
+			 studentChannel002 = "";
+			 referenceChannel001 = "";
+			 referenceChannel002 = "";
+			 mergeChannel001 = "";
+			 mergeChannel002 = "";
+			*/
+			 
+			/* impossible
 			Map<String,String> channelMap = new HashMap<String,String>();
 			channelMap.put("stuC001", studentChannel001);
 			channelMap.put("stuC002", studentChannel002);
@@ -65,7 +178,7 @@ public class TaskCycleProcessor {
 			channelMap.put("refC002", referenceChannel002);
 			channelMap.put("merC001", mergeChannel001);
 			channelMap.put("merC002", mergeChannel002);
-			
+			*/
 
 			List<FlowType> flows = tcase.getFlow();
 			for (FlowType flow : flows) {
@@ -83,7 +196,7 @@ public class TaskCycleProcessor {
 					String par1 =oper.getPar1();
 					String par2 =oper.getPar2();
 					String returnChannel = oper.getReturn();
-
+					
 						/* --- Flow Branch --- */						
 						String flowType = flow.getType();	
 						if("studentFlow".equals(flowType)){
@@ -93,10 +206,10 @@ public class TaskCycleProcessor {
 							switch (operationType) {
 							case "XSLTransform": {
 								System.out.println("................ XSLTransform ");
-								//Student files
-								String fullXSLPathInZip = sdir + sfile1;
-								String fullXMLPathInZip = sdir + sfile2;
-									
+								//Student files									
+								String fullXSLPathInZip = operationParameterValue(par1);
+								String fullXMLPathInZip = operationParameterValue(par2);
+								
 								System.out.println("                 XSL file: " + fullXSLPathInZip);
 								System.out.println("                 XML file: " + fullXMLPathInZip);
 								
@@ -111,15 +224,19 @@ public class TaskCycleProcessor {
 								//OPTION String
 								ByteArrayOutputStream resultOutputStream = new ByteArrayOutputStream();
 								String retStr = trans_ctrl.runTransformToString(resultOutputStream,  null,null);
-
+								/*
 								switch (returnChannel) {
 								case "stuC001":
-									studentChannel001 = retStr;
+									studentChannel001 = retStr; //String.valueOf(retStr.toCharArray());
 									break;
 								case "stuC002":
 									studentChannel002 = retStr;
 									break;
 								}
+								*/
+								setChannelStringValue(returnChannel, retStr);
+								
+								//System.out.println("????????MAP TEST stuC001: " + channelMap.get("stuC001"));
 								
 							}
 								break;
@@ -137,8 +254,11 @@ public class TaskCycleProcessor {
 							case "XSLTransform": {
 								System.out.println("................ XSLTransform ");
 								//Reference files
-								String fullXSLPathInZip = rdir + rfile1;
-								String fullXMLPathInZip = rdir + rfile2;
+								//String fullXSLPathInZip = rdir + rfile1;
+								//String fullXMLPathInZip = rdir + rfile2;
+								
+								String fullXSLPathInZip = operationParameterValue(par1);
+								String fullXMLPathInZip = operationParameterValue(par2);
 									
 								System.out.println("                 XSL file: " + fullXSLPathInZip);
 								System.out.println("                 XML file: " + fullXMLPathInZip);
@@ -154,7 +274,7 @@ public class TaskCycleProcessor {
 								//OPTION String
 								ByteArrayOutputStream resultOutputStream = new ByteArrayOutputStream();
 								String retStr = trans_ctrl.runTransformToString(resultOutputStream,  null,null);
-								
+								/*
 								switch (returnChannel) {
 								case "refC001":
 									referenceChannel001 = retStr;
@@ -162,7 +282,8 @@ public class TaskCycleProcessor {
 								case "refC002":
 									referenceChannel002 = retStr;
 									break;
-								}
+								} */
+								setChannelStringValue(returnChannel, retStr);
 								
 							}
 								break;
@@ -186,7 +307,7 @@ public class TaskCycleProcessor {
 								System.out.println("................ StringCompare ");
 								String arg1str = null;
 								String arg2str = null;
-							/*	*/
+							/*	
 								switch (par1) {
 								case "refC001":
 									arg1str = referenceChannel001;
@@ -201,10 +322,11 @@ public class TaskCycleProcessor {
 									arg1str = studentChannel002;
 									break;
 								}
-							
+							*/
+								arg1str = getChannelStringValue(par1);
 								//arg1str = channelMap.get(par1);
-								System.out.println("==??????" + channelMap.get(par1) + "?????");
-								
+								//System.out.println("==??????" + channelMap.get(par1) + "?????");
+								/*
 								switch (par2) {
 								case "refC001":
 									arg2str = referenceChannel001;
@@ -219,6 +341,8 @@ public class TaskCycleProcessor {
 									arg2str = studentChannel002;
 									break;
 								}
+								*/
+								arg2str = getChannelStringValue(par2);
 								
 								//if (referenceChannel001.equals(studentChannel001))
 								String retStr = null;
@@ -231,7 +355,7 @@ public class TaskCycleProcessor {
 								}
 								
 								System.out.println("===============" + retStr + "=============\n");
-								
+								/*
 								switch (returnChannel) {
 								case "merC001":
 									mergeChannel001 = retStr;
@@ -239,8 +363,8 @@ public class TaskCycleProcessor {
 								case "merC002":
 									mergeChannel002 = retStr;
 									break;
-								}
-								
+								} */
+								setChannelStringValue(returnChannel, retStr);
 							}
 								break;
 							case "NA": {
